da4956b31a647a1d41ec9b1aab34fb9f2ec3a027
revobrera <rev.obrera@gmail.com>
2022-12-21 18:56:07 -0500

    added docstring to views request

--- StellarMapWeb/apiApp/views.py
+++ StellarMapWeb/apiApp/views.py
@@ -7,12 +7,27 @@ from .helpers import SiteChecker
 
 @api_view(['GET'])
 def check_url(request, url):
+    """
+    Check the reachability of the given URL.
+
+    Parameters:
+        url (str): The URL to check.
+
+    Returns:
+        Response: A JSON response containing the reachability status of the URL.
+    """
     checker = SiteChecker()
     result = checker.check_url(url)
     return Response({"reachable": result})
 
 @api_view(['GET'])
 def check_all_urls(request):
+    """
+    Check the reachability of all URLs in the sites_dict.
+
+    Returns:
+        Response: A JSON response containing the reachability status of each URL.
+    """
     sites_dict = {
         "stellar_github": "https://github.com/stellar",
         "stellar_org": "https://www.stellar.org",
@@ -25,4 +40,5 @@ def check_all_urls(request):
         results[site] = checker.check_url(url)
 
     return Response(results)
+
 
