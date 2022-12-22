4f0f7ad3c24f1362fc10f11a9170438972c86aff
revobrera <rev.obrera@gmail.com>
2022-12-21 19:00:30 -0500

    added docstring to urls

--- StellarMapWeb/apiApp/urls.py
+++ StellarMapWeb/apiApp/urls.py
@@ -1,6 +1,27 @@
 from django.urls import path
 
 urlpatterns = [
-    path('check_url/<str:url>/', check_url, name='check_url'),
-    path('check_all_urls/', check_all_urls, name='check_all_urls'),
+    path(
+        'v1/check_url/<str:url>/',
+        check_url,
+        name='check_url',
+        docstring="""Check the reachability of the given URL.
+        
+        Parameters:
+            url (str): The URL to check.
+        
+        Returns:
+            Response: A JSON response containing the reachability status of the URL.
+        """
+    ),
+    path(
+        'v1/check_all_urls/',
+        check_all_urls,
+        name='check_all_urls',
+        docstring="""Check the reachability of all URLs in the sites_dict.
+        
+        Returns:
+            Response: A JSON response containing the reachability status of each URL.
+        """
+    ),
 ]
