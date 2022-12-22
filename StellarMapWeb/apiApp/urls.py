f5a30d35da7998d87508cf8b75d4846f9f1f684d
revobrera <rev.obrera@gmail.com>
2022-12-22 12:20:26 -0500

    removed docstring from re_path

--- StellarMapWeb/apiApp/urls.py
+++ StellarMapWeb/apiApp/urls.py
@@ -1,29 +1,17 @@
 from django.urls import re_path
 from apiApp import views
 
+
 app_name = 'apiApp'
 urlpatterns = [
     re_path(
         'check_url/<str:url>/',
         views.check_url,
-        name='check_url',
-        docstring="""Check the reachability of the given URL.
-        
-        Parameters:
-            url (str): The URL to check.
-        
-        Returns:
-            Response: A JSON response containing the reachability status of the URL.
-        """
+        name='check_url'
     ),
     re_path(
         'check_all_urls/',
         views.check_all_urls,
-        name='check_all_urls',
-        docstring="""Check the reachability of all URLs in the sites_dict.
-        
-        Returns:
-            Response: A JSON response containing the reachability status of each URL.
-        """
+        name='check_all_urls'
     ),
 ]
