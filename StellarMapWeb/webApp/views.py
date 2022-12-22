208fed3f5a4f2b3a8248169c16f6db0346074221
revobrera <rev.obrera@gmail.com>
2022-12-22 17:11:30 -0500

    corrected search_view to point to webApp/search

--- StellarMapWeb/webApp/views.py
+++ StellarMapWeb/webApp/views.py
@@ -4,4 +4,4 @@ def search_view(request):
   context = {
     'search_variable': 'Hello World!'
   }
-  return render(request, 'search.html', context)
+  return render(request, 'webApp/search.html', context)
