155d9d003fcdbc328dfba694ac9a9b1cd5584982
revobrera <rev.obrera@gmail.com>
2022-12-21 19:29:39 -0500

    revised main url path

--- StellarMapWeb/StellarMapWeb/urls.py
+++ StellarMapWeb/StellarMapWeb/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('apiApp.urls')),
+    url(r'^(?i)chief/', admin.site.urls),
+    url(r'^(?i)api/v1/', include('apiApp.urls')),
 ]
