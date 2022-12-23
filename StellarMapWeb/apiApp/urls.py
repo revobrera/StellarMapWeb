abc264b69d906619ad726be36a448c8f6c29f50e
revobrera <rev.obrera@gmail.com>
2022-12-23 14:26:59 -0500

    added swagger api documentation template

--- StellarMapWeb/apiApp/urls.py
+++ StellarMapWeb/apiApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
 from apiApp import views
+from django.views.generic import TemplateView
 
 
 app_name = 'apiApp'
@@ -15,4 +16,8 @@ urlpatterns = [
         name='check_all_urls'
     ),
     re_path('set_network/<str:network>/', views.set_network, name='set_network'),
+    re_path('docs/', TemplateView.as_view(
+        template_name='documentation.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
