e850264ea0d2e5961ee3558d7ddba96ab60ede59
revobrera <rev.obrera@gmail.com>
2022-12-27 17:31:58 -0500

    revised urls and views for swagger ui

--- StellarMapWeb/apiApp/urls.py
+++ StellarMapWeb/apiApp/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 app_name = 'apiApp'
 urlpatterns = [
+    re_path('api/v1/docs/', views.documentation_view, name='swagger-ui'),
     re_path(
         'api/v1/check_url/<str:url>/',
         views.check_url,
@@ -16,6 +17,5 @@ urlpatterns = [
         name='check_all_urls'
     ),
     re_path('api/v1/set_network/<str:network>/', views.set_network, name='set_network'),
-    re_path('api/v1/docs/', TemplateView.as_view(template_name='apiApp/documentation.html'), name='swagger-ui'),
     re_path('api/v1/lineage/network/<str:network>/stellar_address/<str:stellar_account_address>/', views.lineage_stellar_account, name='lineage_stellar_account'),
 ]
