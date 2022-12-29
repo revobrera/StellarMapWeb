a643e1df07025d3d8e1e629ee234489b572d0dde
revobrera <rev.obrera@gmail.com>
2022-12-29 16:23:19 -0500

    added gunicorn_config.py module file

---
+++ StellarMapWeb/gunicorn_config.py
@@ -0,0 +1,23 @@
+import os
+
+bind = "unix:/run/gunicorn.sock"
+workers = 3
+user = "revobrera"
+group = "revobrera"
+loglevel = "debug"
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
+
+# Set the path to the Django project and settings module
+django_project_path = "/home/revobrera/StellarMapWeb/StellarMapWeb"
+django_settings_module = "StellarMapWeb.settings"
+
+# Set the environment variables for the Django project
+os.environ["DJANGO_SETTINGS_MODULE"] = django_settings_module
+os.environ["PYTHONPATH"] = django_project_path
+
+# Set the working directory to the Django project path
+working_directory = django_project_path
+
+# Set the Django wsgi application
+django_wsgi_application = "StellarMapWeb.wsgi:application"
