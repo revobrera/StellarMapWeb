3731db15e6aa3e8f26dbf1f8472eb37033383f85
revobrera <rev.obrera@gmail.com>
2022-12-30 16:32:47 -0500

    added settings will redirect all HTTP requests to HTTPS, set the HSTS header to a long value to enable HTTP Strict Transport Security, and set the Content Security Policy header to a strong value to improve security

--- StellarMapWeb/StellarMapWeb/settings.py
+++ StellarMapWeb/StellarMapWeb/settings.py
@@ -161,3 +161,21 @@ LOGGING = {
 
 # swagger
 REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema' }
+
+# Redirect all HTTP requests to HTTPS
+SECURE_SSL_REDIRECT = True
+
+# Set the X-Forwarded-Proto header to https if the request came through a reverse proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Set the HSTS header to a long value to enable HTTP Strict Transport Security
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# Set the Content Security Policy header to a strong value
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
+
