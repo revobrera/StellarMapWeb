c9f6aa89ab3b6ea63d099017028f14029090f979
revobrera <rev.obrera@gmail.com>
2023-01-09 17:18:15 -0500

    added urls, templates, static for radial tidy tree

--- StellarMapWeb/radialTidyTreeApp/views.py
+++ StellarMapWeb/radialTidyTreeApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+
+def radial_tidy_tree_view(request):
+  context = {
+    'radial_tidy_tree_variable': 'Hello World!'
+  }
+  return render(request, 'webApp/radial_tidy_tree.html', context)
