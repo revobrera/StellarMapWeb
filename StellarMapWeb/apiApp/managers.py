d614442d8ca12b69eb4d7675045f793461a3b9df
revobrera <rev.obrera@gmail.com>
2023-02-04 01:23:55 -0500

    removed unused code

--- StellarMapWeb/apiApp/managers.py
+++ StellarMapWeb/apiApp/managers.py
@@ -1,11 +1,9 @@
-import pandas as pd
 import sentry_sdk
 from apiApp.helpers.sm_conn import AsyncCassandraConnectionsHelpers
 from apiApp.helpers.sm_datetime import StellarMapDateTimeHelpers
-from apiApp.models import (ManagementCronHealthHistory, ManagementCronHealth,
-                           UserInquirySearchHistory, StellarCreatorAccountLineage)
-from decouple import config
-from django.db import connections
+from apiApp.models import (ManagementCronHealth,
+                           StellarCreatorAccountLineage,
+                           UserInquirySearchHistory)
 
 
 class UserInquirySearchHistoryManager():
@@ -165,129 +163,6 @@ class StellarCreatorAccountLineageManager():
             )
             
         except Exception as e:
-            sentry_sdk.capture_exception(e)
-            raise e
-
-
-class ManagementCronHealthHistoryManager():
-
-    def get_queryset(self, **kwargs):
-        """
-        Returns a queryset filtered by the given keyword arguments.
-        
-        :param kwargs: keyword arguments to filter the queryset by
-        :return: a filtered queryset
-        
-        """
-        
-        try:
-            return ManagementCronHealthHistory.objects.filter(**kwargs).first()
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
-            raise e
-
-    def create_cron_health(self, request, *args, **kwargs):
-        """
-        Creates a cron_health with the given information
-        
-        :param request: the request object
-        :param args: additional positional arguments
-        :param kwargs: additional key-value arguments
-        :return: the created cron_health
-        """
-
-        try:
-            # get datetime object
-            dt_helpers = StellarMapDateTimeHelpers()
-            dt_helpers.set_datetime_obj()
-            date_obj = dt_helpers.get_datetime_obj()
-
-            # add the created_at field to the request
-            request['created_at'] = date_obj
-
-            return ManagementCronHealthHistory.objects.create(**request)
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
-            raise e
-
-    def update_cron_health(self, id, status):
-        """
-        Updates an cron_health with the given id.
-
-        :param id: the id of the cron_health to update
-        :param status: the status of the cron_health to update
-        :return: the updated cron_health
-        """
-        try:
-            # get datetime object
-            dt_helpers = StellarMapDateTimeHelpers()
-            dt_helpers.set_datetime_obj()
-            date_obj = dt_helpers.get_datetime_obj()
-
-            # get the cron_health instance
-            cron_health = self.get_queryset(id=id)
-            
-            return cron_health.update(
-                status = status,
-                updated_at = date_obj
-            )
-            
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
-            raise e
-
-    async def get_latest_record(self, cron_name):
-        """
-        Returns the most recent record for the given cron_name.
-
-        :param cron_name: the name of the cron job
-        :return: the most recent record for the cron_name
-        """
-        try:
-            return ManagementCronHealthHistory.objects.filter(cron_name=cron_name).order_by('-created_at').first()
-            # conn_helpers = AsyncCassandraConnectionsHelpers()
-            # cql_query = "SELECT cron_name, status, created_at FROM management_cron_health_history limit 171;"
-
-            # await conn_helpers.set_cql_query(cql_query)
-            # await conn_helpers.connect()
-            # await conn_helpers.execute_cql()
-
-            # result_df = conn_helpers.result_df
-
-            # # convert to dictionary and orient as records
-            # latest_record_dict = result_df.to_dict('records')
-
-            # await conn_helpers.close_connection()
-
-            # return latest_record_dict
-            
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
-            raise e
-
-    async def get_distinct_cron_names(self):
-        try:
-            # query all latest distinct cron names
-            conn_helpers = AsyncCassandraConnectionsHelpers()
-            cql_query = "SELECT cron_name FROM management_cron_health_history limit 171;"
-
-            await conn_helpers.set_cql_query(cql_query)
-            await conn_helpers.connect()
-            await conn_helpers.execute_cql()
-
-            result_df = conn_helpers.result_df
-
-            # using pandas to drop duplicates
-            result_df = result_df.drop_duplicates()
-
-            # convert to dictionary and orient as records
-            cron_names_dict = result_df.to_dict('records')
-
-            await conn_helpers.close_connection()
-
-            return cron_names_dict
-        except Exception as e:
-            # log the error to Sentry
             sentry_sdk.capture_exception(e)
             raise e
 
--- StellarMapWeb/apiApp/models.py
+++ StellarMapWeb/apiApp/models.py
@@ -37,115 +37,6 @@ class BaseModel(DjangoCassandraModel):
         get_pk_field = "id"
         abstract = True
 
-class StellarAccountInquiryHistory(DjangoCassandraModel):
-    """
-    A model for storing the history of user requests to examine a Stellar account.
-
-    Attributes:
-        stellar_account (str): a field that stores the address of the Stellar account
-        network_name (str): a field that stores the network the Stellar account belongs to
-        status (str): a field that stores the status of the request with a max length of 20 and a default value of 'pending'. The valid choices for the status field are 'pending', 'in_progress', and 'completed'.
-
-    Note: 
-        To prevent multiple requests for the same Stellar account, ask the user if they want to make a new request or retrieve data from previous requests for that Stellar account.
-    """
-
-    __keyspace__ = CASSANDRA_DB_NAME
-    id = cassandra_columns.UUID(primary_key=True, default=uuid.uuid4)
-    stellar_account = cassandra_columns.Text(max_length=56)
-    network_name = cassandra_columns.Text(max_length=9)
-    status = cassandra_columns.Text(max_length=63)
-    created_at = cassandra_columns.DateTime()
-    updated_at = cassandra_columns.DateTime()
-
-    def __str__(self):
-        """ Method to display Stellar account, network and status in the admin django interface.
-        """
-        return 'Stellar Account: ' + self.stellar_account + ' | network: ' + self.network_name + ' | status: ' + self.status
-
-    class Meta:
-        get_pk_field = "id"
-        db_table = 'stellar_account_inquiry_history'
-
-class StellarAccountLineage(DjangoCassandraModel):
-    """
-    A model for storing detailed information about the lineage and creator accounts of the Stellar network.
-    
-    Attributes:
-        account_active (str): a field that stores whether or not the Stellar account is active
-        stellar_creator_account (str): a field that stores the address of the Stellar account creator
-        stellar_account (str): a field that stores the address of the Stellar account
-        stellar_account_created_at (datetime): a field that stores the date and time when the Stellar account was created
-        network_name (str): a field that stores the network name
-        home_domain (str): a field that stores the home domain
-        xlm_balance (float): a field that stores the XLM balance of the Stellar account
-        horizon_accounts_doc_api_href (str): a field that stores the horizon accounts doc api href
-        horizon_accounts_operations_doc_api_href (str): a field that stores the horizon accounts operations doc api href
-        horizon_accounts_effects_doc_api_href (str): a field that stores the horizon accounts effects doc api href
-        stellar_expert_explorer_account_doc_api_href (str): a field that stores the Stellar expert explorer account doc api href
-        status (str): a field that stores the status of the request with a default value of 'pending'. The valid choices for the status field are 'pending', 'in_progress', and 'completed'.
-    """
-
-    __keyspace__ = CASSANDRA_DB_NAME
-    id = cassandra_columns.UUID(primary_key=True, default=uuid.uuid4)
-    account_active = cassandra_columns.Text(max_length=30)
-    stellar_creator_account = cassandra_columns.Text(max_length=56)
-    stellar_account = cassandra_columns.Text(max_length=56)
-    stellar_account_created_at = cassandra_columns.DateTime()
-    network_name = cassandra_columns.Text(max_length=9)
-    home_domain = cassandra_columns.Text(max_length=71)
-    xlm_balance = cassandra_columns.Float()
-    horizon_accounts_doc_api_href = cassandra_columns.Text() # "https://horizon.stellar.org/accounts/{stellar_account}"
-    horizon_accounts_operations_doc_api_href = cassandra_columns.Text() # "https://horizon.stellar.org/operations/{stellar_account}"
-    horizon_accounts_effects_doc_api_href = cassandra_columns.Text() # https://horizon.stellar.org/accounts/{stellar_account}/effects
-    stellar_expert_explorer_account_doc_api_href = cassandra_columns.Text() # https://api.stellar.expert/explorer/{network_name}/account/{stellar_account}
-    status = cassandra_columns.Text(max_length=63)
-    created_at = cassandra_columns.DateTime()
-    updated_at = cassandra_columns.DateTime()
-
-    def __str__(self):
-        """ Method to display Stellar account, network and status in the admin django interface.
-        """
-        return 'Stellar Account: ' + self.stellar_account + ' | network: ' + self.network_name + ' | status: ' + self.status
-
-    class Meta:
-        get_pk_field = "id"
-
-
-class ManagementCronHealthHistory(DjangoCassandraModel):
-    """
-    A model that holds information on the status of each cron job in the system.
-    The initial status of each cron job is set to "HEALTHY". In case of errors,
-    the status will be prefixed with "ERROR_". Before executing a cron job, the
-    most recent record entry is checked to verify its status. If the status is
-    not healthy, the cron job will not execute. This mechanism is in place to
-    support the tenacity library, which implements exponential backoff in case
-    of repeated errors and stops the cron job execution once the maximum number
-    of retries is reached.
-
-    Attributes:
-        id (uuid.UUID): A unique identifier for the record, generated using uuid4 by default.
-        cron_name (str): The name of the cron job. The max length is 71.
-        status (str): The health status of the cron job. The default status is "HEALTHY", and if there is an error, it is prefixed with "ERROR_". The max length is 63.
-        created_at (datetime.datetime): The creation time of the record.
-        updated_at (datetime.datetime): The last update time of the record.
-    """
-    __keyspace__ = CASSANDRA_DB_NAME
-    id = cassandra_columns.UUID(primary_key=True, default=uuid.uuid4)
-    cron_name = cassandra_columns.Text(max_length=71)
-    status = cassandra_columns.Text(max_length=63)
-    reason = cassandra_columns.Text()
-    created_at = cassandra_columns.DateTime()
-    updated_at = cassandra_columns.DateTime()
-
-    def __str__(self):
-        """ Method to display cron name and status in the admin django interface.
-        """
-        return 'Cron Name: ' + self.cron_name + ' | Status: ' + self.status
-
-    class Meta:
-        get_pk_field = "id"
-
 
 class UserInquirySearchHistory(DjangoCassandraModel):
     """
@@ -174,7 +65,7 @@ class UserInquirySearchHistory(DjangoCassandraModel):
         return 'Stellar Account: ' + self.stellar_account + ' | network: ' + self.network_name + ' | status: ' + self.status
 
     class Meta:
-        db_table = 'stellar_account_inquiry_history'
+        db_table = 'user_inquiry_search_history'
         get_pk_field = "id"
 
 class StellarCreatorAccountLineage(DjangoCassandraModel):
@@ -219,7 +110,7 @@ class StellarCreatorAccountLineage(DjangoCassandraModel):
         return 'Stellar Account: ' + self.stellar_account + ' | network: ' + self.network_name + ' | status: ' + self.status
 
     class Meta:
-        db_table = 'stellar_account_lineage'
+        db_table = 'stellar_creator_account_lineage'
         get_pk_field = "id"
 
 
