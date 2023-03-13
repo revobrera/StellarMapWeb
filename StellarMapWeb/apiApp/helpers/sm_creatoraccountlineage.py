c771e176a634b1b6634c117ad260ecbfa948e002
revobrera <rev.obrera@gmail.com>
2023-03-13 02:30:35 -0400

    added async_update_from_operations_raw_data(), parse_operations_creator_account(), cron_collect_account_lineage_creator

--- StellarMapWeb/apiApp/helpers/sm_creatoraccountlineage.py
+++ StellarMapWeb/apiApp/helpers/sm_creatoraccountlineage.py
@@ -7,7 +7,7 @@ from django.http import HttpRequest
 
 class StellarMapCreatorAccountLineageHelpers:
 
-    def async_update_from_raw_data(self, client_session, lin_queryset, *args, **kwargs):
+    def async_update_from_accounts_raw_data(self, client_session, lin_queryset, *args, **kwargs):
 
         try:
 
@@ -34,6 +34,39 @@ class StellarMapCreatorAccountLineageHelpers:
                 'home_domain': home_domain,
                 'xlm_balance': native_balance,
                 'status': 'DONE_UPDATING_FROM_RAW_DATA'
+            }
+
+            lineage_manager.update_lineage(id=lin_queryset.id, request=request)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+
+
+    def async_update_from_operations_raw_data(self, client_session, lin_queryset, *args, **kwargs):
+
+        try:
+
+            # Create an instance of the manager
+            lineage_manager = StellarCreatorAccountLineageManager()
+
+            # update status to IN_PROGRESS
+            lineage_manager.update_status(id=lin_queryset.id, status='IN_PROGRESS_UPDATING_FROM_OPERATIONS_RAW_DATA')
+
+            # Create an instance of Astra Document
+            astra_document = AstraDocument()
+            astra_document.set_datastax_url(datastax_url=lin_queryset.horizon_accounts_operations_doc_api_href)
+            response_json = astra_document.get_document()
+
+            # Create an instance of StellarMapHorizonAPIParserHelpers
+            api_parser = StellarMapHorizonAPIParserHelpers()
+            api_parser.set_datastax_response(datastax_response=response_json)
+            creator_dict = api_parser.parse_operations_creator_account(stellar_account=lin_queryset.stellar_account)
+
+            # update lineage record
+            request = HttpRequest()
+            request.data = {
+                'stellar_creator_account': creator_dict.funder,
+                'stellar_account_created_at': creator_dict.created_at,
+                'status': 'DONE_UPDATING_FROM_OPERATIONS_RAW_DATA'
             }
 
             lineage_manager.update_lineage(id=lin_queryset.id, request=request)
